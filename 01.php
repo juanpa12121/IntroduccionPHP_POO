eeca61c1a3b6bd21980a3145b2cf515cd872141e
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2021-02-14 14:25:23 -0500

    Crear una clase e instanciarla

--- 01.php
+++ 01.php
@@ -1,4 +1,40 @@
 <?php include 'includes/header.php';
 
+// Crear clase
+class Producto{
+    //Definir atributos o propiedades
+    public $nombre;
+    public $precio;
+    public $disponible;
+}
+
+// Instanciar clase
+$producto = new Producto();
+
+//Asignar valores
+$producto->nombre = "Tablet";
+$producto->precio = 200;
+$producto->disponible = true;
+
+//Acceder a sus valores
+var_dump($producto->nombre);
+// var_dump($producto->precio);
+// var_dump($producto->disponible);
+
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
+
+
+$producto2 = new Producto();
+$producto2->nombre = "Celular";
+$producto2->precio = 800;
+$producto2->disponible = false;
+
+echo "<pre>";
+var_dump($producto2);
+echo "</pre>";
+
+
 
 include 'includes/footer.php';
