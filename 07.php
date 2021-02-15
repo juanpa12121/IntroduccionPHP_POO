95f0ac087b8e2fbff121183f4371485ecb4d5aa0
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2021-02-15 18:43:43 -0500

    POLIMORFISMO
    *POLIMORFISMO: Los objetos pueden tener diferentes formas
    Diferentes clases IMPLEMENTAN una misma INTERFACE
     Es como la unión de HERENCIA e INTERFACE
    
    Hay una clase hija de X clase padre, y que también implementa la interfaz, que implementa su padre
    Todas las funciones que estén en la INTERFAZ, deben estar implementada minimo en una clase.
    Pero si se ueden tener métodos en las clases, metodos que NO estén definidos en la interfaz

--- 07.php
+++ 07.php
@@ -1,4 +1,52 @@
 <?php include 'includes/header.php';
 
+interface TransporteInterfaz{ //Dice que funciones hay y que retorna
+    public function getInfo() : string;
+    public function getRuedas() : int;
+}
+
+//Clase PADRE (Sólo de PLANTILLA, por eso abstract)
+class Transporte implements TransporteInterfaz { //Clase que implementa la interfaz
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {    
+    }
+
+    public function getInfo() : string { //Retornará STRING
+        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas.";
+    }
+
+    public function getRuedas() : int {
+        return $this->ruedas;
+    }
+}
+
+class Automovil extends Transporte implements TransporteInterfaz {
+    public function __construct(protected int $ruedas, protected int $capacidad, protected string $color)
+    {    
+    }
+    //Metodo para reescribirlo
+    public function getInfo() : string { //Retornará STRING
+        return "El transporte AUTO tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas y tiene el color " . $this->color;
+    }
+
+    //Metodo que NO existe en la interfaz (SI SE PUEDE TENER)
+    public function getColor() : string {
+        return "El color es: " . $this->color;
+    }
+}
+
+echo "<pre>";
+var_dump($transporte = new Transporte(8, 20) );
+var_dump($auto = new Automovil(4, 4, "Rojo") );
+
+echo $transporte->getInfo();
+echo "<hr>";
+
+echo $auto->getInfo();
+echo "<hr>";
+
+echo $auto->getColor();
+echo "</pre>";
+
 
 include 'includes/footer.php';
