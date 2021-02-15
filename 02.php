de7a55209dda1510a18a35bd01122394862eb52f
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2021-02-15 15:50:15 -0500

    Métodos estáticos

--- 02.php
+++ 02.php
@@ -5,26 +5,36 @@ include 'includes/header.php';
 // this evita que se revuelvan los objetos y que cada atributo que se va añadiendo a la clase,
 //se guarde donde debería
 
-// ENCAPSULACIÓN
+//**********  MÉTODOS ESTÁTICOS *********************************************
+    //No requiere instanciarse
 
 // Crear clase
+//Al poner public en la CLASE: Lo hace obligatorio
 class Producto{
 
-    //**Public: Se puede acceder y modificar en cualquier lugar (En CLASE o en OBJETO)
-    //**Protected: Se puede acceder/modificar unicamente en la clase */
-    //**Private: Solo miembros de la misma clase pueden acceder a el */
-        //Private casi no cambia nada
-        // (parecido a protected cuando se tiene 1 CLASE), tiene mas efecto en HERENCIA,
-    // PHP 8 (menos código) (CLASE) ********************************************************************
-    public function __construct(protected string $nombre, public int $precio, public bool $disponible)
+    public $imagen;
+    // Tambien se pueden tener variables estaticas
+    public static $imagenPlaceholder = "Imagen.jpg"; //Ej: Img que estará por default
+
+    public function __construct(protected string $nombre, public int $precio, public bool $disponible, string $imagen)
     {
+        if($imagen){ //Si hay una nueva imagen
+            self::$imagenPlaceholder = $imagen; //Sobreescribirla
+        }
+    }
+
+    public static function obtenerImagenProducto(){
+        return self::$imagenPlaceholder;
+    }
+
+    public static function obtenerProducto(){
+        echo "Obteniendo datos del producto...";
     }
 
     //Metodos
     public function mostrarProducto() : void{
         echo "El producto es: " . $this->nombre . " y su precio es de: $" . $this->precio;
     }
-    // This hace ref a los atributos que fueron agrgados en el objeto creado en la clase
 
     public function getNombre() : string{ //PARA PROTECTED (Poder acceder a el)
         return $this->nombre;
@@ -33,16 +43,19 @@ class Producto{
     public function setNombre(string $nombre){//Sirve para modificar el objeto creado previamente
         $this->nombre = $nombre;
     }
-
- 
 }
 
+//echo Producto::obtenerProducto();
+//echo Producto::obtenerImagenProducto();
+
 // Instanciar clase
-$producto = new Producto('Tablet', 200, true); 
+$producto = new Producto('Tablet', 200, true, ""); //Se queda la imagen por DEFAULT
 //$producto->mostrarProducto(); //CON PUBLIC
 
 //$producto->nombre = "Nuevo nombre" //NO SE PUEDE USAR CON PROTECTED
 // SE DEBE CREAR UN NUEVO MÉTODO DENTRO DE LA CLASE
+
+echo $producto->obtenerImagenProducto();
 
 echo $producto->getNombre(); //CON PROTECTED
 $producto->setNombre("Nuevo nombre");
@@ -64,9 +77,11 @@ var_dump($producto);
 echo "</pre>";
 
 
-$producto2 = new Producto('Monitor Curvo', 300, true);
+$producto2 = new Producto('Monitor Curvo', 300, true, "MonitorCurvo.jpg"); //Entra al if y sobreescribe la img del método estático
 //$producto2->mostrarProducto(); //CON PUBLIC
 echo $producto2->getNombre(); //CON PROTECTED
+
+echo $producto->obtenerImagenProducto();
 
 // $producto2->nombre = "Celular";
 // $producto2->precio = 800;
@@ -75,7 +90,6 @@ echo $producto2->getNombre(); //CON PROTECTED
 // echo "<pre>";
 // var_dump($producto2);
 // echo "</pre>";
-
 
 
 include 'includes/footer.php';
