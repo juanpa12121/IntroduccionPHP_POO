656323428452fb9f3c85b6b646ce9c128da43070
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2021-02-15 21:27:32 -0500

    Namespaces
    En PHP no se puede tener 2 clases que se llamen igual, de igual forma en una carpeta 2 archivos no pueden llamarse igual
    Permiten dar subnombres a las clases

--- 08.php
+++ 08.php
@@ -1,21 +1,36 @@
 <?php include 'includes/header.php';
-// Se recomienda tener UNAclase por archivo
-
-
+// Se recomienda tener UNA clase por archivo
 
 //NO NECESARIO PARA INCLUIR
+//Incluir otras clases
 // require 'clases/Clientes.php';
 // require 'clases/Detalles.php';
 
+use App\Clientes;
+use App\Detalles;
+
 // Incluir las otras clases
 function mi_autoload($clase){ //Para importar automatica/ las clases que voy requiriendo
-    require  __DIR__ . '/clases/' . $clase . '.php';
+    //echo $clase; //R= App\Detalles, Pero hay que quitarle el App
+    $partes = explode('\\', $clase); //Busque en el string "App\Detalles", la \
+    //Crea un array con App y Detalles, en las pos 0 y 1 respectiva/
+
+    require  __DIR__ . '/clases/' . $partes[1] . '.php';
 }
 
 spl_autoload_register('mi_autoload');
 
+// class Clientes { //Clase que se repite
+//     public function __construct()
+//     {
+//         echo "Desde 08.php que contiene los clientes";
+//     }
+// }
+
 // Con lo anterior, ya podremos instanciar de Clientes y Detalles
+// Ya que autoload automatica/ itera y busca las clases que vamos a instanciar
 $detalles = new Detalles();
-$clientes = new Clientes();
+$clientes = new Clientes(); //Clase ya no choca con la class Clientes de este archivo .php
+//$clientes2 = new Clientes();  //Instancia de Clase que se repite
 
 include 'includes/footer.php';
