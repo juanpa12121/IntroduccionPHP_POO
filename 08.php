701fe373221629778c47e7ad8c88bca7970afa58
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2021-02-15 19:13:40 -0500

    AUTOLOAD de clases
    Autoload de clases:
    Se recomienda tener un archivo por CLASE
    Autoload permite que no se tenga que poner muchos require de clases, ej:
    // require 'clases/Clientes.php';
    // require 'clases/Detalles.php';
    Autoload importa automaticamente las clases
    
    Autoload busca las clases según las vayas importando

--- 08.php
+++ 08.php
@@ -1,4 +1,21 @@
 <?php include 'includes/header.php';
+// Se recomienda tener UNAclase por archivo
 
+
+
+//NO NECESARIO PARA INCLUIR
+// require 'clases/Clientes.php';
+// require 'clases/Detalles.php';
+
+// Incluir las otras clases
+function mi_autoload($clase){ //Para importar automatica/ las clases que voy requiriendo
+    require  __DIR__ . '/clases/' . $clase . '.php';
+}
+
+spl_autoload_register('mi_autoload');
+
+// Con lo anterior, ya podremos instanciar de Clientes y Detalles
+$detalles = new Detalles();
+$clientes = new Clientes();
 
 include 'includes/footer.php';
---
+++ clases/Clientes.php
@@ -0,0 +1,8 @@
+<?php
+
+class Clientes{
+    public function __construct()
+    {
+        echo "Desde la Clase Clientes.php";
+    }
+}
